0d19e72ad0ae9cc65d39b07e28b7bce313deb04c
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2021-01-05 23:00:58 +0900

    fix

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"time"
 )
 
 type user struct {
@@ -16,9 +15,15 @@ type user struct {
 
 const seq = 1
 
+var users = map[int]*user{}
+
 func main() {
 	e := echo.New()
-	e.PUT("/create", createUser)
+	e.POST("/users/create", createUser)
+	e.GET("/users/:id", getUser)
+	e.PUT("/users/:id", updateUser)
+
+	e.Logger.Fatal(e.Start(":3000"))
 }
 
 func createUser(c echo.Context) error {
@@ -28,7 +33,24 @@ func createUser(c echo.Context) error {
 	if err := c.Bind(u); err != nil {
 		return err
 	}
-
+	users[u.ID] = u
 	return c.JSON(http.StatusOK, u)
+}
 
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	iid, _ := strconv.Atoi(id)
+
+	return c.JSON(http.StatusOK, users[iid])
+}
+
+func updateUser(c echo.Context) error {
+	u := new(user)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	id := c.Param("id")
+	iid, _ := strconv.Atoi(id)
+	users[iid].Name = u.Name
+	return c.JSON(http.StatusOK, users[iid])
 }
